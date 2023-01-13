db37f61dbedb40553870ad837412bf1dd61e630b
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-01-13 07:27:23 -0300

    feat: when success log it, when fail requeue and try again

--- Application/Logger/Program.cs
+++ Application/Logger/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Api.Models;
+using MessageService;
+using MessageService.Config;
 
-Console.WriteLine("Hello, World!");
+using var consumer = new Consumer<MessageData>();
+
+consumer.Configure(
+    new ProducerConnectionSettings("localhost", "user", "password", 5672),
+    new ProducerQueueSettings("events-queue", "events-ex", "save-event")
+);
+
+consumer.OnReceived += data =>
+{
+    try
+    {
+        Console.WriteLine($"* [{data.Id}] SAVING LOG \"{data.Message}\"");
+        Thread.Sleep(500);
+        
+        var random = new Random();
+
+        var randomNumber = random.Next();
+
+        if (randomNumber % 3 == 0)
+            throw new Exception("SOME FAIL");
+        
+        Console.WriteLine($"+ [{data.Id}] LOG SAVED, ACK ");
+    }
+    catch
+    {
+        Console.WriteLine($"! [{data.Id}] LOG SAVE FAILED, NACK ");
+        throw;
+    }
+};
+
+consumer.StartListen();
+
+Console.WriteLine("press [enter] to quit");
+Console.ReadKey();
